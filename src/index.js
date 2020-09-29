6d615919e3b39e6ee3fc721ac6c8d06e1bc8e89b
korkutsk <xloaddd@gmail.com>
2020-09-29 21:45:34 +0300

    Added redux-saga; Added a saga auth file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import './index.css';
 import App from './App';
@@ -11,8 +12,9 @@ import registerServiceWorker from './registerServiceWorker';
 import builderBurgerReducer from './store/reducer/burgerBuilder';
 import orderReducer from './store/reducer/order';
 import authReducer from './store/reducer/auth';
+import { watchAuth } from './store/sagas/index';
 
-const composeEnchancer = process.env.NODE_ENV === 'development' 
+const composeEnchancer = process.env.NODE_ENV === 'development'
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true, traceLimit: 25 })
     : null || compose;
 
@@ -22,7 +24,13 @@ const rootReducer = combineReducers({
     auth: authReducer
 });
 
-const store = createStore(rootReducer, composeEnchancer(applyMiddleware(thunk)));
+const sagaMiddeware = createSagaMiddleware();
+
+const store = createStore(rootReducer, composeEnchancer(
+    applyMiddleware(thunk, sagaMiddeware)
+));
+
+sagaMiddeware.run(watchAuth);
 
 const app = (
     <Provider store={store}>
